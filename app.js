dde217817680ab44f7348517cd66775b5c5d735a
MlodziutkiS <sados4f@gmail.com>
2025-02-24 15:55:41 +0100

    less commit

--- app.js
+++ app.js
@@ -25,16 +25,33 @@ app.get('/play/newRoom', (req,res)=>{
 app.get('/about', (req,res)=>{
   res.sendFile(path.join(__dirname, "./about.html"))
 })
+app.get('/name', (req,res)=>{
+  res.sendFile(path.join(__dirname, "./name.html"))
+})
+
+let rooms=[]
+let users=[{id: 'skibidi', name: 'toilet'}]
 
 io.on('connection', (socket) => {
+   // users.push({socket.id})
     console.log('a user connected');
-    socket.on("newRoom",(packet)=>{
-      
+    socket.on("newRoom",(params)=>{
+      console.log("duration", params.dura, "pressure", params.pres, "secure", params.secureBool)
+      rooms.push({id: socket.id, duration: params.dura, pressure: params.pres, secure: params.secureBool})
+      console.table(rooms);
     })
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
+    socket.on("name",(name)=>{
+     // users[socket.id].name= name;
+    })
+
   });
+
+app.get('/rooms', (req,res)=>{
+  res.send(rooms);
+})
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
